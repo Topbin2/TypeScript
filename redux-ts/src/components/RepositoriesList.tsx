184151154a4ef9130f249c5ac1defb14f8a0b1bc
MoSangBin <mosangbin94@gmail.com>
2022-07-04 10:35:39 +0900

    ts

--- redux-ts/src/components/RepositoriesList.tsx
+++ redux-ts/src/components/RepositoriesList.tsx
@@ -1,11 +1,18 @@
-import React, { FormEvent, useState } from 'react';
+import React, { useState } from "react";
+import { useActions } from "../hooks/useActions";
+import { useTypedSelector } from "../hooks/useTypedSelector";
 
 const RepositoriesList = () => {
   const [term, setTerm] = useState("");
+  const { searchRepositories } = useActions();
+  const { data, error, loading } = useTypedSelector(
+    (state) => state.repositories
+  );
 
   const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-  }
+    searchRepositories(term);
+  };
 
   return (
     <div>
@@ -17,6 +24,9 @@ const RepositoriesList = () => {
         />
         <button>Search</button>
       </form>
+      {error && <h3>{error}</h3>}
+      {loading && <h3>Loading...</h3>}
+      {!error && !loading && data.map((name) => <div key={name}>{name}</div>)}
     </div>
   );
 };
