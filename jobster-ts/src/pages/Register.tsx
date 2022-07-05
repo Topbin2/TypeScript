8cc9d2694fa04e27343c05d14a3bd523a10b0e1c
MoSangBin <mosangbin94@gmail.com>
2022-07-06 00:47:03 +0900

    ts

--- jobster-ts/src/pages/Register.tsx
+++ jobster-ts/src/pages/Register.tsx
@@ -1,10 +1,49 @@
-import React from 'react';
+import { useState, useEffect, FormEvent, useCallback } from "react";
+import { Logo } from "../components";
+import Wrapper from "../assets/wrappers/RegisterPage";
+
+const initialState = {
+  name: "",
+  email: "",
+  password: "",
+  isMember: true,
+};
 
 const Register = () => {
+  const [values, setValues] = useState(initialState);
+
+  const handleChange = useCallback((e: FormEvent<HTMLInputElement>) => {
+    // console.log(e.target.value);
+  }, []);
+
+  const onSubmit = useCallback((e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(e);
+  }, []);
+
   return (
-    <div>
-      Register
-    </div>
+    <Wrapper>
+      <form className="form" onSubmit={onSubmit}>
+        <Logo />
+        <h3>Login</h3>
+        {/* name field */}
+        <div className="form-row">
+          <label htmlFor="name" className="form-label">
+            name
+          </label>
+          <input
+            className="form-input"
+            type="text"
+            name="name"
+            value={values.name}
+            onChange={handleChange}
+          />
+        </div>
+        <button className="btn btn-block">
+          submit
+        </button>
+      </form>
+    </Wrapper>
   );
 };
 
