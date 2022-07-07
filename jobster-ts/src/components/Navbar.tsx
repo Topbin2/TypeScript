eba04fb2a2a36dab594dbc5e23764166b3d06f14
MoSangBin <mosangbin94@gmail.com>
2022-07-07 22:45:33 +0900

    ts

--- jobster-ts/src/components/Navbar.tsx
+++ jobster-ts/src/components/Navbar.tsx
@@ -1,9 +1,47 @@
 import Wrapper from "../assets/wrappers/Navbar";
+import { FaAlignLeft, FaUserCircle, FaCaretDown } from "react-icons/fa";
+import { useAppSelector, useAppDispatch } from "../hooks";
+import Logo from "./Logo";
 
 const Navbar = () => {
+  const { user } = useAppSelector((state) => state.user);
+  const dispatch = useAppDispatch();
+
   return (
     <Wrapper>
-      <h2>Navbar</h2>
+      <div className="nav-center">
+        <button
+          className="toggle-btn"
+          type="button"
+          onClick={() => console.log("toggle sidebar")}
+        >
+          <FaAlignLeft />
+        </button>
+        <div>
+          <Logo />
+          <h3 className="logo-text">dashboard</h3>
+        </div>
+        <div className="btn-container">
+          <button
+            type="button"
+            className="btn"
+            onClick={() => console.log("toggle logout dropdown")}
+          >
+            <FaUserCircle />
+            {user?.name}
+            <FaCaretDown />
+          </button>
+          <div className="dropdown show-dropdown">
+            <button
+              type="button"
+              className="dropdown-btn"
+              onClick={() => console.log("logout user")}
+            >
+              logout
+            </button>
+          </div>
+        </div>
+      </div>
     </Wrapper>
   );
 };
