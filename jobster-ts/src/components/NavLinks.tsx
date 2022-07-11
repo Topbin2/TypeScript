ddf8592eb902111a7c8620d5c0589c25e945a04a
MoSangBin <mosangbin94@gmail.com>
2022-07-11 14:12:45 +0900

    ts

---
+++ jobster-ts/src/components/NavLinks.tsx
@@ -0,0 +1,31 @@
+import { NavLink } from "react-router-dom";
+import links from "../utils/links";
+
+interface IProps {
+  toggleSidebar: () => void;
+}
+
+const NavLinks: React.FC<IProps> = ({ toggleSidebar }) => {
+  return (
+    <div className="nav-links">
+      {links.map((link) => {
+        const { id, text, path, icon } = link;
+        return (
+          <NavLink
+            to={path}
+            className={({ isActive }) =>
+              isActive ? "nav-link active" : "nav-link"
+            }
+            key={id}
+            onClick={toggleSidebar}
+          >
+            <span className="icon">{icon}</span>
+            {text}
+          </NavLink>
+        );
+      })}
+    </div>
+  );
+};
+
+export default NavLinks;
--- jobster-ts/src/components/SmallSidebar.tsx
+++ jobster-ts/src/components/SmallSidebar.tsx
@@ -5,15 +5,14 @@ import Wrapper from "../assets/wrappers/SmallSidebar";
 import { useAppDispatch, useAppSelector } from "../hooks";
 import { toggleSidebar } from "../reducers/userSlice";
 import Logo from "./Logo";
-import links from "../utils/links";
-import { NavLink } from "react-router-dom";
+import NavLinks from "./NavLinks";
 
 const SmallSidebar = () => {
   const { isSidebarOpen } = useAppSelector((state) => state.user);
   const dispatch = useAppDispatch();
 
   const toggle = useCallback(() => {
-    dispatch(toggleSidebar);
+    dispatch(toggleSidebar());
   }, [dispatch]);
 
   return (
@@ -30,24 +29,7 @@ const SmallSidebar = () => {
           <header>
             <Logo />
           </header>
-          <div className="nav-links">
-            {links.map((link) => {
-              const { id, text, path, icon } = link;
-              return (
-                <NavLink
-                  to={path}
-                  className={({ isActive }) =>
-                    isActive ? "nav-link active" : "nav-link"
-                  }
-                  key={id}
-                  onClick={toggle}
-                >
-                  <span className="icon">{icon}</span>
-                  {text}
-                </NavLink>
-              );
-            })}
-          </div>
+          <NavLinks toggleSidebar={toggle} />
         </div>
       </div>
     </Wrapper>
