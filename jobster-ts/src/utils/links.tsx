e76e4c6cdf0307fd83138ef88da3165714836de7
MoSangBin <mosangbin94@gmail.com>
2022-07-11 13:52:32 +0900

    ts

---
+++ jobster-ts/src/utils/links.tsx
@@ -0,0 +1,20 @@
+import { IoBarChartSharp } from "react-icons/io5";
+import { MdQueryStats } from "react-icons/md";
+import { FaWpforms } from "react-icons/fa";
+import { ImProfile } from "react-icons/im";
+
+interface ILinks {
+  id: number;
+  text: string;
+  path: string;
+  icon: JSX.Element;
+}
+
+const links: Array<ILinks> = [
+  { id: 1, text: "stats", path: "/", icon: <IoBarChartSharp /> },
+  { id: 2, text: "all jobs", path: "all-jobs", icon: <MdQueryStats /> },
+  { id: 3, text: "add job", path: "add-job", icon: <FaWpforms /> },
+  { id: 4, text: "profile", path: "profile", icon: <ImProfile /> },
+];
+
+export default links;
