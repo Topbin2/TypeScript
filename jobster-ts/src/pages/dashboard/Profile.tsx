4cc6c285ea1a0b723be0d8f26f8dee802d5d24f5
MoSangBin <mosangbin94@gmail.com>
2022-07-11 15:38:14 +0900

    ts

--- jobster-ts/src/pages/dashboard/Profile.tsx
+++ jobster-ts/src/pages/dashboard/Profile.tsx
@@ -1,4 +1,85 @@
+import { ChangeEvent, FormEvent, useState } from "react";
+import { FormRow } from "../../components";
+import Wrapper from "../../assets/wrappers/DashboardFormPage";
+import { useAppDispatch, useAppSelector } from "../../hooks";
+import { toast } from "react-toastify";
+import { useCallback } from "react";
+
+interface IUserData {
+  name: string;
+  email: string;
+  lastName: string;
+  location: string;
+}
+
 const Profile = () => {
-  return <h1>Profile</h1>;
+  const { isLoading, user } = useAppSelector((state) => state.user);
+  const dispatch = useAppDispatch();
+
+  const [userData, setUserData] = useState<IUserData>({
+    name: user?.name || "",
+    email: user?.email || "",
+    lastName: user?.lastName || "",
+    location: user?.location || "",
+  });
+
+  const handleSubmit = useCallback(
+    (e: FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
+      const { name, email, lastName, location } = userData;
+      if (!name || !email || !lastName || !location) {
+        toast.error("please fill out all fields");
+        return;
+      }
+    },
+    [userData]
+  );
+
+  const handleChange = useCallback(
+    (e: ChangeEvent<HTMLInputElement>) => {
+      const name = e.target.name;
+      const value = e.target.value;
+      setUserData({ ...userData, [name]: value });
+    },
+    [userData]
+  );
+
+  return (
+    <Wrapper>
+      <form className="form" onSubmit={handleSubmit}>
+        <h3>profile</h3>
+        <div className="form-center">
+          <FormRow
+            type="text"
+            name="name"
+            value={userData.name}
+            handleChange={handleChange}
+          />
+          <FormRow
+            type="text"
+            labelText="last name"
+            name="lastName"
+            value={userData.lastName}
+            handleChange={handleChange}
+          />
+          <FormRow
+            type="email"
+            name="email"
+            value={userData.email}
+            handleChange={handleChange}
+          />
+          <FormRow
+            type="text"
+            name="location"
+            value={userData.location}
+            handleChange={handleChange}
+          />
+          <button type="submit" className="btn btn-block" disabled={isLoading}>
+            {isLoading ? "Please Wait..." : "save changes"}
+          </button>
+        </div>
+      </form>
+    </Wrapper>
+  );
 };
 export default Profile;
