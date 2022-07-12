5edf9b5d237ca75ad6f97cf4723fe31fe74e5c32
MoSangBin <mosangbin94@gmail.com>
2022-07-13 00:13:34 +0900

    ts

--- jobster-ts/src/pages/dashboard/AddJob.tsx
+++ jobster-ts/src/pages/dashboard/AddJob.tsx
@@ -2,10 +2,38 @@ import { FormRow } from "../../components";
 import Wrapper from "../../assets/wrappers/DashboardFormPage";
 import { useAppDispatch, useAppSelector } from "../../hooks";
 import { toast } from "react-toastify";
+import { ChangeEvent, FormEvent, useCallback } from "react";
 
 const AddJob = () => {
-  const { isLoading, position, company, jobLocation, jobType, jobTypeOptions } =
-    useAppSelector((state) => state.job);
+  const {
+    isLoading,
+    position,
+    company,
+    jobLocation,
+    jobType,
+    jobTypeOptions,
+    status,
+    statusOptions,
+    isEditing,
+    editJobId,
+  } = useAppSelector((state) => state.job);
+
+  const handleSubmit = useCallback(
+    (e: FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
+      if (!position || !company || !jobLocation) {
+        toast.error("Please fill out all fields");
+        return;
+      }
+    },
+    [position, company, jobLocation]
+  );
+
+  const handleJobInput = useCallback((e: ChangeEvent<HTMLInputElement>) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    console.log(name, value);
+  }, []);
 
   return <h1>AddJobs</h1>;
 };
