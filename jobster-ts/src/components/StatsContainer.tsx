74dfc8373a463de3f370893af4669070e8be9cae
MoSangBin <mosangbin94@gmail.com>
2022-07-20 20:50:01 +0900

    ts

--- jobster-ts/src/components/StatsContainer.tsx
+++ jobster-ts/src/components/StatsContainer.tsx
@@ -1,9 +1,49 @@
+import StatItem from "./StatItem";
+import { FaSuitcaseRolling, FaCalendarCheck, FaBug } from "react-icons/fa";
+import Wrapper from "../assets/wrappers/StatsContainer";
+import { useAppSelector } from "../hooks";
+
+interface Stats {
+  title: string;
+  count: number;
+  icon: JSX.Element;
+  color: string;
+  bcg: string;
+}
 
 const StatsContainer = () => {
+  const { stats } = useAppSelector((state) => state.allJobs);
+
+  const defaultStats: Array<Stats> = [
+    {
+      title: "pending applications",
+      count: stats.pending || 0,
+      icon: <FaSuitcaseRolling />,
+      color: "#e0b040",
+      bcg: "#fcefc7",
+    },
+    {
+      title: "interviews scheduled",
+      count: stats.interview || 0,
+      icon: <FaCalendarCheck />,
+      color: "#647acb",
+      bcg: "#e0e8f9",
+    },
+    {
+      title: "jobs declined",
+      count: stats.pending || 0,
+      icon: <FaBug />,
+      color: "#d66a6a",
+      bcg: "#ffeeee",
+    },
+  ];
+
   return (
-    <h2>
-      StatsContainer
-    </h2>
+    <Wrapper>
+      {defaultStats.map((item, index) => {
+        return <StatItem key={index} {...item} />;
+      })}
+    </Wrapper>
   );
 };
 
