2ab1d45a92247793a9386fa5157b5f464e1d9851
MoSangBin <mosangbin94@gmail.com>
2022-07-23 16:17:11 +0900

    feat: SearchContainer component 추가

--- jobster-ts/src/components/SearchContainer.tsx
+++ jobster-ts/src/components/SearchContainer.tsx
@@ -1,5 +1,66 @@
+import { FormRow, FormRowSelect } from "../components";
+import Wrapper from "../assets/wrappers/SearchContainer";
+import { useAppSelector, useAppDispatch } from "../hooks";
+import { ChangeEvent, FormEvent } from "react";
+
 const SearchContainer = () => {
-  return <h2>SearchContainer</h2>;
+  const { isLoading, search, searchStatus, searchType, sort, sortOptions } =
+    useAppSelector((store) => store.allJobs);
+  const { jobTypeOptions, statusOptions } = useAppSelector(
+    (store) => store.job
+  );
+  const dispatch = useAppDispatch();
+  const handleSearch = (e: ChangeEvent) => {};
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+  };
+
+  return (
+    <Wrapper>
+      <form className="form">
+        <h4>search form</h4>
+        <div className="form-center">
+          {/* search by position */}
+          <FormRow
+            type="text"
+            name="search"
+            value={search}
+            handleChange={handleSearch}
+          />
+          {/* search by status */}
+          <FormRowSelect
+            labelText="status"
+            name="searchStatus"
+            value={searchStatus}
+            handleChange={handleSearch}
+            list={["all", ...statusOptions]}
+          />
+          {/* search by type */}
+          <FormRowSelect
+            labelText="type"
+            name="searchType"
+            value={searchType}
+            handleChange={handleSearch}
+            list={["all", ...jobTypeOptions]}
+          />
+          {/* sort */}
+          <FormRowSelect
+            name="sort"
+            value={sort}
+            handleChange={handleSearch}
+            list={sortOptions}
+          />
+          <button
+            className="btn btn-block btn-danger"
+            disabled={isLoading}
+            onClick={handleSubmit}
+          >
+            clear filters
+          </button>
+        </div>
+      </form>
+    </Wrapper>
+  );
 };
 
 export default SearchContainer;
