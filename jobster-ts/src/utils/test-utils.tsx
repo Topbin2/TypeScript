512ed11341ff59098dad9e221693b36d06d2eba3
MoSangBin <mosangbin94@gmail.com>
2022-08-05 21:19:52 +0900

    fix: test-utils 수정

--- jobster-ts/src/utils/test-utils.tsx
+++ jobster-ts/src/utils/test-utils.tsx
@@ -1,57 +1,10 @@
-// import React, { PropsWithChildren } from "react";
-// import { render } from "@testing-library/react";
-// import type { RenderOptions } from "@testing-library/react";
-// import { configureStore } from "@reduxjs/toolkit";
-// import type { PreloadedState } from "@reduxjs/toolkit";
-// import { Provider } from "react-redux";
-
-// import { AppStore, RootState, setupStore } from "../store/store";
-// import { allJobsReducer } from "../reducers/allJobsSlice";
-// import { jobReducer } from "../reducers/jobSlice";
-// import { userReducer } from "../reducers/userSlice";
-// import { BrowserRouter } from "react-router-dom";
-// // As a basic setup, import your same slice reducers
-
-// // This type interface extends the default options for render from RTL, as well
-// // as allows the user to specify other things such as initialState, store.
-// interface ExtendedRenderOptions extends Omit<RenderOptions, "queries"> {
-//   preloadedState?: PreloadedState<RootState>;
-//   store?: AppStore;
-// }
-
-// export function renderWithProviders(
-//   ui: React.ReactElement,
-//   {
-//     preloadedState = {},
-//     // Automatically create a store instance if no store was passed in
-//     store = setupStore(preloadedState),
-//     ...renderOptions
-//   }: ExtendedRenderOptions = {}
-// ) {
-//   function Wrapper({ children }: PropsWithChildren<{}>): JSX.Element {
-//     return (
-//       <BrowserRouter>
-//         <Provider store={store}>{children}</Provider>
-//       </BrowserRouter>
-//     );
-//   }
-
-//   // Return an object with the store and all of RTL's query functions
-//   return { store, ...render(ui, { wrapper: Wrapper, ...renderOptions }) };
-// }
-
 import React, { PropsWithChildren } from "react";
 import { render as rtlRender } from "@testing-library/react";
-import { configureStore } from "@reduxjs/toolkit";
 import { Provider } from "react-redux";
 import { BrowserRouter as Router } from "react-router-dom";
-import { AppStore, RootState } from "../store/store";
+import setupStore, { AppStore, RootState } from "../store/store";
 import type { RenderOptions } from "@testing-library/react";
 import type { PreloadedState } from "@reduxjs/toolkit";
-
-import { allJobsReducer } from "../reducers/allJobsSlice";
-import { jobReducer } from "../reducers/jobSlice";
-import { userReducer } from "../reducers/userSlice";
 
 interface ExtendedRenderOptions extends Omit<RenderOptions, "queries"> {
   preloadedState?: PreloadedState<RootState>;
@@ -62,10 +15,7 @@ function render(
   ui: React.ReactElement,
   {
     preloadedState = {},
-    store = configureStore({
-      reducer: { user: userReducer, allJobs: allJobsReducer, job: jobReducer },
-      preloadedState,
-    }),
+    store = setupStore(preloadedState),
     ...renderOptions
   }: ExtendedRenderOptions = {}
 ) {
