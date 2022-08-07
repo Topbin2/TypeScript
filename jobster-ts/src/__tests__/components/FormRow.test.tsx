9785d6ffedde43cc99bd13862866a4690b146c0e
MoSangBin <mosangbin94@gmail.com>
2022-08-07 17:58:08 +0900

    test: FormRow Component test

---
+++ jobster-ts/src/__tests__/components/FormRow.test.tsx
@@ -0,0 +1,32 @@
+import userEvent from "@testing-library/user-event";
+import { FormRow } from "../../components";
+import { render, screen } from "../../utils/test-utils";
+
+describe("FormRow Component", () => {
+  it("prop에 맞게 렌더링 된다.", () => {
+    const fn = jest.fn();
+
+    render(
+      <FormRow
+        type="text"
+        name="position"
+        value={undefined}
+        handleChange={fn}
+      />
+    );
+
+    const input = screen.getByRole("textbox");
+    expect(input).toBeInTheDocument();
+
+    expect(input).toHaveAttribute("id", "position");
+    expect(input).toHaveAttribute("type", "text");
+    expect(input).toHaveAttribute("name", "position");
+    expect(input).toHaveAttribute("value", "");
+
+    userEvent.type(input, "sangbin");
+    expect(fn).toBeCalledTimes(7);
+
+    userEvent.click(input);
+    expect(input).toHaveStyle("border: 2px inset");
+  });
+});
